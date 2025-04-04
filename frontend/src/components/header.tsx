c1869c8436b9d501c8260081e43b65a16ab9ec7a
badmagick329 <uzairfarooqi@gmail.com>
2025-04-04 22:20:38 +0100

    feat: add frosted glass navbar

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -9,11 +9,10 @@ export default function Header() {
   const pathname = usePathname();
 
   return (
-    <div className='sticky top-0 z-10 flex flex-col'>
+    <div className='sticky top-0 z-10 flex flex-col border-b border-border/40 bg-background/80 shadow-sm backdrop-blur-md'>
       <header className='flex justify-center gap-4 py-4'>
         <nav className='xs:text-xl flex justify-center gap-4 text-lg sm:gap-12 sm:text-2xl lg:gap-24'>
           <NavbarLink href='/' pathname={pathname} />
-          {/* <NavbarLink href='/posts' pathname={pathname} /> */}
           <NavbarLink href='/about' pathname={pathname} />
           <NavbarLink href='/contact' pathname={pathname} />
           <ModeToggle />
